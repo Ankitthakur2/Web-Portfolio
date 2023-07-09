b551c9682be1791613084930b80f734a474afea7
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-07-10 04:08:50 +0530

    Update Contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,25 +3,57 @@ import "./contact.css";
 
 const Contact = () => {
   return (
-    <div class="container">
-  <h1>Contact</h1>
-  <form target="_blank" action="https://formsubmit.co/ankit.nita.mca@gmail.com" method="POST">
-    <div class="form-group">
-      <div class="form-row">
-        <div class="col">
-          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+    <div className="form1">
+      <div className="form-container">
+        <div className="title1">
+          <h1> Contact</h1>
         </div>
-        <div class="col">
-          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        <div className="content1">
+          <form
+            target="_blank"
+            action="https://formsubmit.co/ankit.nita.mca@gmail.com"
+            method="POST"
+          >
+            <div class="form-group">
+              <div class="form-row">
+                <div class="col">
+                  <input
+                    type="text"
+                    name="name"
+                    class="form-control"
+                    placeholder="Full Name"
+                    required
+                  />
+                </div>
+                <div class="col">
+                  <input
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="Email Address"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <textarea
+                placeholder="Your Message"
+                class="form-control"
+                name="message"
+                rows="7"
+                required
+              ></textarea>
+            </div>
+            <input type="hidden" name="_captcha" value="false" />
+            <input type="hidden" name="_template" value="table" />
+            <button type="submit" class="btn btn-lg btn-primary btn-block">
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     </div>
-    <div class="form-group">
-      <textarea placeholder="Your Message" class="form-control" name="message" rows="7" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-lg btn-primary btn-block">Submit Form</button>
-  </form>
-</div>
   );
 };
 
