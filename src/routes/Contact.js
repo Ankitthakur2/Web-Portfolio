8936f37d8615e0be55b6bf9ffb3409d31be8f5e5
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-07-10 04:39:39 +0530

    Update Contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -14,40 +14,33 @@ const Contact = () => {
             action="https://formsubmit.co/ankit.nita.mca@gmail.com"
             method="POST"
           >
-            <div class="form-group">
-              <div class="form-row">
-                <div class="col">
-                  <input
-                    type="text"
-                    name="name"
-                    class="form-control"
-                    placeholder="Full Name"
-                    required
-                  />
-                </div>
-                <div class="col">
-                  <input
-                    type="email"
-                    name="email"
-                    class="form-control"
-                    placeholder="Email Address"
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <textarea
-                placeholder="Your Message"
-                class="form-control"
-                name="message"
-                rows="7"
-                required
-              ></textarea>
-            </div>
+            <input
+              type="text"
+              name="name"
+              class="form-control"
+              placeholder="Full Name"
+              required
+            />
+
+            <input
+              type="email"
+              name="email"
+              class="form-control"
+              placeholder="Email Address"
+              required
+            />
+
+            <textarea
+              placeholder="Your Message"
+              class="form-control"
+              name="message"
+              rows="7"
+              required
+            ></textarea>
+
             <input type="hidden" name="_captcha" value="false" />
             <input type="hidden" name="_template" value="table" />
-            <button type="submit" class="btn btn-lg btn-primary btn-block">
+            <button type="submit" class="button-7">
               Send Message
             </button>
           </form>
