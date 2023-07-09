138c5491da5f1a1266b89a6e31a56b9dcf9aa1e4
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-07-10 00:04:48 +0530

    Update Contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -5,22 +5,25 @@ const Contact = () => {
   return (
    
 <div class="container">
-  <h1>Contact</h1>
-  <form target="_blank" action="https://formsubmit.co/" method="POST">
+  <h1>Connect</h1>
+  <form target="_blank" action="https://formsubmit.co/ankit.nita.mca@gmail.com" method="POST">
     <div class="form-group">
       <div class="form-row">
         <div class="col">
-          <input type="text" name="name" class="form-control" placeholder="Full Name" required />
+          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
         </div>
         <div class="col">
-          <input type="email" name="email" class="form-control" placeholder="Email Address" required />
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
         </div>
       </div>
     </div>
     <div class="form-group">
       <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
     </div>
-    <button type="submit" class="btn btn-lg btn-primary btn-block">Send Message</button>
+    <input type="hidden" name="_subject" value="New submission!">
+    <input type="hidden" name="_template" value="table">
+    <input type="hidden" name="_captcha" value="false">
+    <button type="submit" class="btn btn-lg btn-primary btn-block">send Message</button>
   </form>
 </div>
    
