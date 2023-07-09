fd9103eefe3fa71028d5c0964d838a3c4530a8a4
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-07-10 00:56:21 +0530

    Update Contact.js

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -4,7 +4,7 @@ import "./contact.css";
 const Contact = () => {
   return (
    <div class="form1">
- <div class="container">
+ <div >
   <h1>Contact</h1>
   <form action="https://formsubmit.co/ankit.nita.mca@gmail.com" method="POST">
     <div class="form-group">
