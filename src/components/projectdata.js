936069bb960302d9ccae603c3fbddc8766392887
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-08-19 06:28:51 +0000

    Update 7 files

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -1,6 +1,27 @@
 const cardData = {
+  card4: {
+    imgSrc: require("../assets/Quiz.png"),
+    title: "• Quiz Web App",
+    description: `Interactive quizzes using React and API. Developed a user-friendly quiz application with React components
+    and API integration,`,
+  },
+  card5: {
+    imgSrc: require("../assets/Movie.png"),
+    title: "Movie Review Web App",
+    description: `Movie information and user reviews using React and API. Developed a responsive movie review platform
+    with React and API integration.`,
+  },
+  card3: {
+    imgSrc: require("../assets/Portfolio.png"),
+    title: "Professional Portfolio",
+    description: `Developed a personal portfolio website using React, HTML, CSS, 
+    and JavaScript to showcase my skills, projects, and 
+    professional experience. The portfolio website serves 
+    as an interactive platform to highlight my background, 
+    and expertise.`,
+  },
   card1: {
-    imgSrc: "../assets/pic2.png",
+    imgSrc: require("../assets/pic2.png"),
     title: "Blog",
     description: `A Blogging web App in which you can
      create and publish a blog. 
@@ -8,24 +29,21 @@ const cardData = {
     comment on blogs.
     Tech Used: 
     HTML, CSS, JavaScript, Bootstrap, 
-    EJS, Node.js, Express.js, MongoDB.`
+    EJS, Node.js, Express.js, MongoDB.`,
   },
   card2: {
-    imgSrc: "../assets/pic1.png",
+    imgSrc: require("../assets/MusicPlayer.png"),
     title: "Music Player",
     description: `A Web Music Player App for 
     Listening to Music on the Browser.
-    Tech Used: HTML, CSS, JavaScript,Bootstrap.`
+    Tech Used: HTML, CSS, JavaScript,Bootstrap.`,
   },
-  card3: {
-    imgSrc: "../assets/pic3.png",
-    title: "Professional Portfolio",
-    description: `Developed a personal portfolio website using React, HTML, CSS, 
-    and JavaScript to showcase my skills, projects, and 
-    professional experience. The portfolio website serves 
-    as an interactive platform to highlight my background, 
-    and expertise.`
-  }
+  card6: {
+    imgSrc: require("../assets/Dictionary.png"),
+    title: "Dictionary Web App",
+    description: `Look up word meanings using an API. Created a user-friendly dictionary interface using HTML, CSS, and
+    JavaScript.`,
+  },
 };
 
 export default cardData;
