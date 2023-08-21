462d0b90b3ea33337b2d0bfb170b7fdb67338494
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-08-21 18:35:28 +0530

    Update projectdata.js

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -4,12 +4,14 @@ const cardData = {
     title: "Quiz Web App",
     description: `Interactive quizzes using React and API. Developed a user-friendly quiz application with React components
     and API integration,`,
+    url:"https://quiz-web-app-wine.vercel.app/",
   },
   card5: {
     imgSrc: require("../assets/Movie.png"),
     title: "Movie Review Web App",
     description: `Movie information and user reviews using React and API. Developed a responsive movie review platform
     with React and API integration.`,
+    url:"https://responsive-movie-recommendation.vercel.app/",
   },
   card3: {
     imgSrc: require("../assets/Portfolio.png"),
@@ -19,6 +21,7 @@ const cardData = {
     professional experience. The portfolio website serves 
     as an interactive platform to highlight my background, 
     and expertise.`,
+    url:"https://ankitportfoli.vercel.app/",
   },
   card1: {
     imgSrc: require("../assets/pic2.png"),
@@ -30,6 +33,7 @@ const cardData = {
     Tech Used: 
     HTML, CSS, JavaScript, Bootstrap, 
     EJS, Node.js, Express.js, MongoDB.`,
+    url:"https://lets-blog.up.railway.app/",
   },
   card2: {
     imgSrc: require("../assets/MusicPlayer.png"),
@@ -37,12 +41,14 @@ const cardData = {
     description: `A Web Music Player App for 
     Listening to Music on the Browser.
     Tech Used: HTML, CSS, JavaScript,Bootstrap.`,
+    url:"https://listenmusicwithankit.netlify.app/",
   },
   card6: {
     imgSrc: require("../assets/Dictionary.png"),
     title: "Dictionary Web App",
     description: `Look up word meanings using an API. Created a user-friendly dictionary interface using HTML, CSS, and
     JavaScript.`,
+    url:"https://ankitthakur2.github.io/Dictionary/",
   },
 };
 
