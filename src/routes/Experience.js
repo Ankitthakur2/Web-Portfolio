a10d04c9774f8c3e367c1720158ad3efc64ed625
Ankit Thakur <62520500+Ankitthakur2@users.noreply.github.com>
2023-10-26 19:14:02 +0530

    Update Experience.js

--- src/routes/Experience.js
+++ src/routes/Experience.js
@@ -71,7 +71,7 @@ const Experience = () => {
         </div>
       </div>
       <div className="about-second">
-       <h1>Working Experience </h1>
+       <p> About</p>
         <div className="intro">
           <p> Born and Brought up in</p>
           <img src={MGIF} alt="Mountain GIF" className="mgif" />
